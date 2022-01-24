e45c1898b25c6299a29304276bbca3805a882860
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-24 10:13:35 -0500

    Relational API implemented interface API

---
+++ src/RelationalTwitterAPI.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+class RelationalTwitterAPI implements ITwitterAPI{
+
+  @Override
+  public void postTweet(Tweet t) {
+
+  }
+
+  @Override
+  public List<Tweet> getTimeline(Integer user_id) {
+    return null;
+  }
+
+  @Override
+  public List<Integer> getFollowers(Integer user_id) {
+    return null;
+  }
+
+  @Override
+  public List<Integer> getFollowees(Integer user_id) {
+    return null;
+  }
+
+  @Override
+  public List<Tweet> getTweets(Integer user_id) {
+    return null;
+  }
+}
