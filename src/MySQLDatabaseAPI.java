61762b619d4a9206facb38253efffa8fa5b18e3c
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-25 18:48:36 -0500

    Implemented getFollowees

--- src/MySQLDatabaseAPI.java
+++ src/MySQLDatabaseAPI.java
@@ -1,57 +1,109 @@
-import java.sql.JDBCType;
+import java.util.ArrayList;
 import java.util.List;
 
 import java.sql.*;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 public class MySQLDatabaseAPI implements IDatabaseAPI {
 
-  private DBUtils utils;
+  private DBUtils dbUtils;
 
   @Override
   public void postTweet(Tweet t) {
-    String sql = "INSERT INTO tweets (tweet_id, user_id, tweet_ts, tweet_text) VALUES (NULL, ?, NULL, ?)";
-
-    int key = -1;
-    try {
-
-      // get connection and initialize statement
-      Connection con = this.utils.getConnection(); // get the active connection
-      PreparedStatement statement = con.prepareStatement(sql);
-      statement.setInt(2, t.getUserId());
-      statement.setString(4, t.getText());
-
-      int rowsInserted = statement.executeUpdate();
-      if (rowsInserted > 0) {
-        System.out.println("A new user was inserted successfully!");
-      }
-      Statement stmt = con.createStatement();
-      stmt.executeUpdate(sql);
-
-      // Cleanup
-      stmt.close();
-
-    } catch (SQLException e) {
-      System.err.println("ERROR: Could not insert record: "+sql);
-      System.err.println(e.getMessage());
-      e.printStackTrace();
-    }
+//    String sql = "INSERT INTO tweets (tweet_id, user_id, tweet_ts, tweet_text) VALUES (NULL, ?, NULL, ?)";
+    String sql = "INSERT INTO tweets (tweet_id, user_id, tweet_ts, tweet_text) VALUES" +
+            "('NULL','"+t.getUserId()+"','NULL','"+t.getText()+"')";
+    dbUtils.insertOneRecord(sql);
   }
 
   @Override
   public List<Tweet> getTimeline(Integer user_id) {
+    // select userid and text from tweets where the user id is one of the user ids of the given user's following list
+    // SELECT user_id, tweet_text FROM tweets t
+
+
+//    String sql = "select doctor_id, lastname, firstname, new_patients, specialty, h.name hospital "+
+//            "from doctor d join specialty using (specialty_id) " +
+//            "join hospital h using (hospital_id) "+
+//            "where new_patients = 1 "+
+//            "and specialty like '"+specialty.toUpperCase()+"'";
+//
+//    List<Doctor> doctors = new ArrayList<Doctor>();
+//
+//
+//    try {
+//      // get connection and initialize statement
+//      Connection con = dbu.getConnection();
+//      Statement stmt = con.createStatement();
+//      ResultSet rs = stmt.executeQuery(sql);
+//      while (rs.next() != false)
+//        doctors.add(new Doctor(rs.getInt("doctor_id"), rs.getString("lastname"), rs.getString("firstname"),
+//                rs.getBoolean("new_patients"), rs.getString("specialty"), rs.getString("hospital")));
+//      rs.close();
+//      stmt.close();
+//    } catch (SQLException e) {
+//      System.err.println(e.getMessage());
+//      e.printStackTrace();
+//    }
+//
+//    return doctors;
     return null;
   }
 
   @Override
   public List<Integer> getFollowers(Integer user_id) {
+//    String sql = "select user_id"+
+//            "from tweets t join follows f using (specialty_id) " +
+//            "join hospital h using (hospital_id) "+
+//            "where new_patients = 1 "+
+//            "and specialty like '"+specialty.toUpperCase()+"'";
+//
+//    List<Doctor> doctors = new ArrayList<Doctor>();
+//
+//
+//    try {
+//      // get connection and initialize statement
+//      Connection con = dbu.getConnection();
+//      Statement stmt = con.createStatement();
+//      ResultSet rs = stmt.executeQuery(sql);
+//      while (rs.next() != false)
+//        doctors.add(new Doctor(rs.getInt("doctor_id"), rs.getString("lastname"), rs.getString("firstname"),
+//                rs.getBoolean("new_patients"), rs.getString("specialty"), rs.getString("hospital")));
+//      rs.close();
+//      stmt.close();
+//    } catch (SQLException e) {
+//      System.err.println(e.getMessage());
+//      e.printStackTrace();
+//    }
+//
+//    return doctors;
+
     return null;
   }
 
   @Override
   public List<Integer> getFollowees(Integer user_id) {
-    return null;
+    // select follows_id from follows where user_id = user_id
+    String sql = "SELECT follows_id"+
+            "FROM follows" +
+            "WHERE user_id = " + user_id;
+
+    List<Integer> followees = new ArrayList<Integer>();
+
+    try {
+      // get connection and initialize statement
+      Connection con = dbUtils.getConnection();
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next() != false)
+        followees.add(rs.getInt("follows_id"));
+      rs.close();
+      stmt.close();
+    } catch (SQLException e) {
+      System.err.println(e.getMessage());
+      e.printStackTrace();
+    }
+
+    return followees;
   }
 
   @Override
@@ -61,11 +113,11 @@ public class MySQLDatabaseAPI implements IDatabaseAPI {
 
   @Override
   public void authenticate(String url, String user, String password) {
-
+    this.dbUtils = new DBUtils(url, user, password);
   }
 
   @Override
   public void closeConnection() {
-
+    this.dbUtils.closeConnection();
   }
 }
