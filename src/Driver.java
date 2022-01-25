13ee608817a5116d292ad315046177847e74fbe8
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-25 00:15:53 -0500

    Using scanner to read csv

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Driver {
   private ITwitterAPI api;
 
@@ -5,9 +8,16 @@ public class Driver {
     this.api = api;
   }
 
-  // Driver program to read tweets from tweets.csv
-  public void readTweets() {
-
+  // Driver program to read tweets from tweets.csv and add them to MySQL Database
+  public void readTweets() throws FileNotFoundException {
+    //parsing a CSV file into Scanner class constructor
+    Scanner sc = new Scanner(new File("../hw1_data/tweets_sample"));
+    sc.useDelimiter(",");   //sets the delimiter pattern
+    while (sc.hasNext())  //returns a boolean value
+    {
+      System.out.print(sc.next());  //find and returns the next complete token from this scanner
+    }
+    sc.close();  //closes the scanner
   }
 
   // Driver program to return a random user's home timeline
@@ -19,5 +29,4 @@ public class Driver {
   public static void main(String[] args) {
 
   }
-
 }
