b17de513a3074d835ddb652ce1fe90f40b6f9589
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-25 13:02:31 -0500

    Added userId field to tweet class

--- src/Tweet.java
+++ src/Tweet.java
@@ -1,8 +1,14 @@
 public class Tweet {
+  private int userId;
   private String text;
 
-  public Tweet(String text) {
+  public Tweet(int userId, String text) {
+    this.userId = userId;
     this.text = text;
+  }
+
+  public int getUserId() {
+    return userId;
   }
 
   public String getText() {
