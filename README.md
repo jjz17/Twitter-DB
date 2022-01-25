cf2b33d03ef1c9df6c90123cd7d6ea8d6970ca7b
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-25 08:47:27 -0500

    Added relevant resource links to README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Twitter-RDB
 DS 4300 HW1: Relational vs. NoSQL Twitter Case Study
+
+## Resources
+### Read CSV Tutorial 
+https://www.javatpoint.com/how-to-read-csv-file-in-java
+
+### Inserting Records into MySQL 
+https://www.vogella.com/tutorials/MySQLJava/article.html
--- src/RelationalTwitterAPI.java
+++ src/RelationalTwitterAPI.java
@@ -1,5 +1,13 @@
 import java.util.List;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
 class RelationalTwitterAPI implements ITwitterAPI{
 
   @Override
