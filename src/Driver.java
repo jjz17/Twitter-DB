678138963718634822543c8f88ba1d2593371824
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-25 00:05:28 -0500

    Created driver class to test programs

---
+++ src/Driver.java
@@ -0,0 +1,18 @@
+public class Driver {
+
+  // Driver program to read tweets from tweets.csv
+  public void readTweets() {
+
+  }
+
+  // Driver program to return a random user's home timeline
+  public void randomHomeTimeline() {
+
+  }
+
+  // Main method
+  public static void main(String[] args) {
+
+  }
+
+}
