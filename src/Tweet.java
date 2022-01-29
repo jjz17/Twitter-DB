11165741e2cd207ff1f8cda6c2c56588a637455b
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-28 20:12:39 -0500

    Created convenience constructor for Tweet class

--- src/Tweet.java
+++ src/Tweet.java
@@ -12,6 +12,13 @@ public class Tweet {
     this.text = text;
   }
 
+  public Tweet(int tweet_id, int user_id, String text, Timestamp timestamp) {
+    this.tweet_id = tweet_id;
+    this.user_id = user_id;
+    this.text = text;
+    this.timestamp = timestamp;
+  }
+
   @Override
   public String toString() {
     return "User: " + this.user_id + ", tweet: " + this.text;
