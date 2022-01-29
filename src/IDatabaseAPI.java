661415bc040d659dd7f9504099ff0a8fe25c63da
Jason Zhang <jasonjzhang17@gmail.com>
2022-01-29 09:33:18 -0500

    Documented IDatabaseAPI

--- src/IDatabaseAPI.java
+++ src/IDatabaseAPI.java
@@ -1,38 +1,38 @@
 import java.util.List;
 
-
+// Interface for Database APIs
 public interface IDatabaseAPI {
 
   /**
    * Post one tweet
-   * @param t
+   * @param t tweet to post
    */
   void postTweet(Tweet t);
 
   /**
-   *
-   * @param user_id
-   * @return
+   * Retrieve the timeline of a given user
+   * @param user_id target user
+   * @return the user's home timeline
    */
   List<Tweet> getTimeline(Integer user_id);
 
   /**
    * Find followers of a given user
-   * @param user_id
+   * @param user_id target user
    * @return a list of user id's
    */
   List<Integer> getFollowers(Integer user_id);
 
   /**
    * Find followees of a given user (who the given user follows)
-   * @param user_id
+   * @param user_id target user
    * @return a list of user id's
    */
   List<Integer> getFollowees(Integer user_id);
 
   /**
    * Find tweets posted by a given user
-   * @param user_id
+   * @param user_id target user
    * @return a list of tweets
    */
   List<Tweet> getTweets(Integer user_id);
@@ -45,9 +45,9 @@ public interface IDatabaseAPI {
 
   /**
    * Set connection settings
-   * @param url
-   * @param user
-   * @param password
+   * @param url target connection url
+   * @param user database user
+   * @param password user password
    */
   void authenticate(String url, String user, String password);
 
