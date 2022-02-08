46a1914a801e480230ac5054f0f1e18ed547448c
Jason Zhang <jasonjzhang17@gmail.com>
2022-02-08 16:44:46 -0500

    Jedis insert speed test

--- src/main/java/JedisTest.java
+++ src/main/java/JedisTest.java
@@ -17,5 +17,18 @@ public class JedisTest {
     jedis.rpush("friends", "Thomas Aquinas");
     // Query is inclusive of indices
     List<String> friends = jedis.lrange("friends", 0, 2);
+
+    String counter_val = jedis.get("counter");
+    int i_counter_val = Integer.parseInt(counter_val);
+    System.out.println(i_counter_val);
+
+    long N = 100000;
+    long startTime = System.nanoTime();
+    for (int i = 0; i < N; i++) {
+      jedis.set("key_" + i, "" + i);
+    }
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime) / 1000000;
+    System.out.println((float) N * 1000.0 / duration + " inserts per second");
   }
 }
