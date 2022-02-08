b0fa9d0ea6097b8b5616783b51a113d16336f56f
Jason Zhang <jasonjzhang17@gmail.com>
2022-02-08 16:40:48 -0500

    Testing Jedis functionality

--- src/main/java/JedisTest.java
+++ src/main/java/JedisTest.java
@@ -1,7 +1,21 @@
+import java.util.List;
+
 import redis.clients.jedis.Jedis;
 
 public class JedisTest {
   public static void main(String[] args) {
+    // Jedis client object
     Jedis jedis = new Jedis();
+
+    jedis.flushAll();
+    jedis.set("hello", "world");
+    jedis.set("counter", "1");
+    jedis.incr("counter");
+
+    jedis.lpush("friends", "joe");
+    jedis.lpush("friends", "mary");
+    jedis.rpush("friends", "Thomas Aquinas");
+    // Query is inclusive of indices
+    List<String> friends = jedis.lrange("friends", 0, 2);
   }
 }
