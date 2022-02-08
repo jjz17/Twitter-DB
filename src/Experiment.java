5095d471e87db5d6ca33fb11cfa15e6f12f00471
Jason Zhang <jasonjzhang17@gmail.com>
2022-02-08 00:04:59 -0500

    Working through Jedis issues

--- src/Experiment.java
+++ src/Experiment.java
@@ -2,6 +2,7 @@ import redis.clients.jedis.Jedis;
 
 public class Experiment {
   public static void main(String[] args) {
-    Jedis jedis = new Jedis("127.0.0.1", 6379);
+//    Jedis jedis = new Jedis("127.0.0.1", 6379);
+    Jedis jedis = new Jedis();
   }
 }
